a8a1ed2b9c086a2388327ff4db2486a74a166f94
Chetan <contact@mrchetan.com>
2025-02-25 19:58:34 +0530

    Initial commit: Setup composer.json and service provider

---
+++ src/Providers/CoreServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Devanox\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        // Register your package's services here.
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Bootstrap your package's services here.
+    }
+}
