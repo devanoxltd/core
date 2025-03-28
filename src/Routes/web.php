e729b5124ca3776d7d455e56c82de252f236c6d1
Chetan <contact@mrchetan.com>
2025-03-28 18:46:49 +0530

    refactor: Simplify installation route definition by using Route::view

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -6,7 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('avatar/{provider}/{username?}', [AvatarController::class, 'index'])
     ->name('avatar');
 if (!isAppInstalled()) {
-    Route::get('install', function () {
-        return view('core::install');
-    })->name('install');
+    Route::view('install', 'core::install')->name('install');
 }
