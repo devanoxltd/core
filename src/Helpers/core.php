b8e43938b2a0ec7b90d8340c8f3dbfa1dda3f653
Mr Chetan <contact@mrchetan.com>
2025-06-22 10:10:45 +0530

    feat: Remove unused module helper functions to streamline codebase

--- src/Helpers/core.php
+++ src/Helpers/core.php
@@ -5,66 +5,10 @@ use Devanox\Core\Support\Module;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 
-if (! function_exists('modulePath')) {
-    function modulePath(string $module, ?bool $fullPath = false, ?bool $enable = false): string
-    {
-        return Module::path($module, $fullPath, $enable);
-    }
-}
-
-if (! function_exists('module')) {
-    function module(): Collection
-    {
-        return Module::get();
-    }
-}
-
 if (! function_exists('modules')) {
     function modules(?bool $disable = false): Collection
     {
         return Module::get()->where('enabled', $disable ? false : true);
-    }
-}
-
-if (! function_exists('moduleExist')) {
-    function moduleExist(string $module): bool
-    {
-        return Module::exist($module);
-    }
-}
-
-if (! function_exists('moduleIsEnabled')) {
-    function moduleIsEnabled(string $module): bool
-    {
-        return Module::isEnabled($module);
-    }
-}
-
-if (! function_exists('moduleIsDisabled')) {
-    function moduleIsDisabled(string $module): bool
-    {
-        return Module::isDisabled($module);
-    }
-}
-
-if (! function_exists('moduleEnable')) {
-    function moduleEnable(string $module): void
-    {
-        Module::enable($module);
-    }
-}
-
-if (! function_exists('moduleDisable')) {
-    function moduleDisable(string $module): void
-    {
-        Module::disable($module);
-    }
-}
-
-if (! function_exists('modulePrefix')) {
-    function modulePrefix(string $module): string
-    {
-        return Module::prefix($module);
     }
 }
 
