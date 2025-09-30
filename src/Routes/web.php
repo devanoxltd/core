6f943b0917c4d6e68fe1f62a920e5ded377909bb
Mr Chetan <contact@mrchetan.com>
2025-09-30 14:43:24 +0530

    refactor: Remove AvatarProvider and related classes for simplification

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
-use Devanox\Core\Http\Controllers\AvatarController;
 use Illuminate\Support\Facades\Route;
 
 if (!isAppInstalled()) {
     Route::view('install', 'core::install')->name('install');
 }
-
-Route::get('avatar/{provider}/{username?}', [AvatarController::class, 'index'])
-    ->name('avatar');
 
 Route::view('license', 'core::license')->name('license');
